7089d217200fd1f48f46d5683105ff6819637799
Santiago Medina <santiagomedina111204@gmail.com>
2024-10-01 16:43:59 -0300

    RecyclerView conectado con el adapter, ahora muestra lista de gastos (Cafe con precio 4000)

--- app/src/main/java/com/example/gastosya/MainActivity.java
+++ app/src/main/java/com/example/gastosya/MainActivity.java
@@ -10,23 +10,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Gasto> listaGastos;
+    private GastoAdapter gastoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Obtener boton
-        Button btnAgregarGasto = findViewById(R.id.btnAgregarGasto);
+        listaGastos = new ArrayList<>();
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewGastos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        gastoAdapter = new GastoAdapter(listaGastos);
+        recyclerView.setAdapter(gastoAdapter);
 
-        // Evento al hacer click
+        Button btnAgregarGasto = findViewById(R.id.btnAgregarGasto);
         btnAgregarGasto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Agregar Gasto deseado", Toast.LENGTH_SHORT).show();
+                agregarGasto("Cafe", 4000.00);
             }
         });
+    }
+
+    private void agregarGasto(String nombre, double cantidad) {
+        listaGastos.add(new Gasto(nombre, cantidad));
+        gastoAdapter.notifyDataSetChanged();
     }
 }
